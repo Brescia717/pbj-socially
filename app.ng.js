51a7c85cc7b26ac040eb7b37602fc29fc34b7ee2
Paul Brescia <brescia717@gmail.com>
2015-08-21 13:15:49 -0400

    1. Changed app.js to app.ng.js for simplification of functions 2. Added form for new parties 3. Added delete functionality for removing created parties

---
+++ app.ng.js
@@ -0,0 +1,30 @@
+Parties = new Mongo.Collection("parties");
+
+if (Meteor.isClient) {
+  angular.module('socially', ['angular-meteor']);
+
+  angular.module('socially').controller('PartiesListCtrl', function ($scope, $meteor) {
+    $scope.parties = $meteor.collection(Parties);
+
+    $scope.remove = function(party){
+      $scope.parties.splice($scope.parties.indexOf(party), 1);
+    };
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert(parties[i]);
+    }
+  });
+}
