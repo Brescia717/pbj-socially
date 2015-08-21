af309b27d313b402d84b20024646e0abb896b7ef
Paul Brescia <brescia717@gmail.com>
2015-08-21 14:54:59 -0400

    1. Removed 'insecure' package 2. Added 'accounts-ui' and 'accounts-password' packages 3. Added config for using username instead of email for user login/signup 4. Added user login/signup 5. Added validation for create/edit/delete for parties

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -1,2 +1,6 @@
 // These are the dependencies (similar to gems in Rails)
 angular.module('socially', ['angular-meteor', 'ui.router']);
+
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+});
