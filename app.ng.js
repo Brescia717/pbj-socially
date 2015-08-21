04b67f2563920811c7cfeeaea120c8fde21d93dd
Paul Brescia <brescia717@gmail.com>
2015-08-21 14:05:24 -0400

    1. Created party-details.ng.html 2. Set up controller logic for party-details page 3. Added a form for modifying a party's name and description, as well as poviding 'reset' and 'cancel' buttons

--- app.ng.js
+++ app.ng.js
@@ -4,17 +4,55 @@ if (Meteor.isClient) {
   // These are the dependencies (similar to gems in Rails)
   angular.module('socially', ['angular-meteor', 'ui.router']);
 
+  // Config
+  angular.module('socially').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+    function($urlRouterProvider, $stateProvider, $locationProvider){
+
+      $locationProvider.html5Mode(true);
+
+      $stateProvider
+        .state('parties', {
+          url: '/parties',
+          templateUrl: 'parties-list.ng.html',
+          controller: 'PartiesListCtrl'
+        })
+        .state('partyDetails', {
+          url: '/parties/:partyId',
+          templateUrl: 'party-details.ng.html',
+          controller: 'PartyDetailsCtrl'
+        });
+
+      $urlRouterProvider.otherwise("/parties");
+    }]);
+
+  // Controllers
   angular.module('socially').controller('PartiesListCtrl', function ($scope, $meteor) {
     // In Ruby, these would be methods
     $scope.parties = $meteor.collection(Parties);
 
-    $scope.remove = function(party){
+    $scope.remove = function(party) {
       $scope.parties.remove(party);
     };
 
-    $scope.removeAll = function(){
+    $scope.removeAll = function() {
       $scope.parties.remove();
-    }
+    };
+  });
+
+  angular.module("socially").controller("PartyDetailsCtrl", function($scope, $stateParams, $meteor) {
+    $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+
+    $scope.save = function() {
+      $scope.party.save().then(function(numberOfDocs){
+          console.log('save success doc affected ', numberOfDocs);
+        }, function(error){
+          console.log('save error', error);
+        });
+    };
+
+    $scope.reset = function () {
+      $scope.party.reset();
+    };
   });
 }
 
